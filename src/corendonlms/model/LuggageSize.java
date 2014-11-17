9a4f7f712102d60682777f0987046f73bd6766c1
epels <emile.pels@hva.nl>
2014-11-17 11:58:19 +0100

    Added LuggageManager and LuggageSizes

---
+++ src/corendonlms/model/LuggageSize.java
@@ -0,0 +1,13 @@
+package corendonlms.model;
+
+/**
+ * Enumeration holding the various sizes luggage can have
+ * 
+ * @author Emile
+ */
+public enum LuggageSize
+{
+    Small,
+    Medium,
+    Large
+}
