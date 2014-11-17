f5a5c753e51159a39467f56f4ed44c66af659618
epels <emile.pels@hva.nl>
2014-11-17 16:40:04 +0100

    Added border to ImageLabel

--- src/corendonlms/view/ImageLabel.java
+++ src/corendonlms/view/ImageLabel.java
@@ -1,7 +1,11 @@
 package corendonlms.view;
 
+import corendonlms.main.CorendonLMS;
+import java.awt.Color;
+import javax.swing.BorderFactory;
 import javax.swing.ImageIcon;
 import javax.swing.JLabel;
+import javax.swing.border.Border;
 
 /**
  * Creates a simple picturebox
@@ -12,8 +16,15 @@ import javax.swing.JLabel;
  */
 public class ImageLabel extends JLabel
 {
+    private static final Color BORDER_COLOR = CorendonLMS.DEFAULT_FORECOLOR;
+    private static final Border BORDER = BorderFactory.createLineBorder(BORDER_COLOR, 5);
+    
     public ImageLabel(String fileName)
     {
         super(new ImageIcon(fileName));
+        
+        setBackground(CorendonLMS.DEFAULT_BACKCOLOR);
+        setBorder(BORDER);
+        setOpaque(true);
     }
 }
